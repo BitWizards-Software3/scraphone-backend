8f74af794374d24688cd2981b9947cdc4a165690
Heosve <juanjosehenao1810@gmail.com>
2023-10-13 00:29:13 -0500

    Manejo de expeciones http en la ruta de scrap

--- routes/scrap_router.py
+++ routes/scrap_router.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from typing import Dict
 from services.scraper_services import scrape_amazon
 
 router = APIRouter()
 
 
 @router.post("/scrape/")
-async def scrape_endpoint(search_term: str):
+async def scrape_endpoint(search_data: Dict[str, str]):
+    # Verifica si el término de búsqueda está presente en los datos
+    if "search_term" not in search_data:
+        raise HTTPException(status_code=422, detail="Falta el término de búsqueda")
+    
+    search_term = search_data["search_term"]
+    
+    # Llama a la función de scraping con el término de búsqueda
     product_data = scrape_amazon(search_term)
+
     return {"productos": product_data}
