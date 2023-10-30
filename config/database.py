5fb8062797dc0b9ea94a55f64c9f185b93d3d73d
MariVR <maricruz.vazquezro@alumno.buap.mx>
2023-10-30 18:50:59 -0500

    create endpoints get, get_all and delete by id

--- config/database.py
+++ config/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Configuración de la URL de conexión a la base de datos MySQL
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:12345@localhost:3306/scraphone"
+#only config Mary userbd is admin1 for other is admin
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin1:12345@localhost:3306/scraphone"
 
 # Creación de una instancia del motor de la base de datos
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- routes/product.py
+++ routes/product.py
@@ -15,6 +15,43 @@ def get_db():
     finally:
         db.close()
 
+# Endpoint para obtener información sobre un producto específico por su ID
+@router.get("/products/{product_id}", response_model=ProductView)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Producto no encontrado")
+    print(product.name)
+
+    # Crear una instancia de ProductView con los campos correspondientes
+    product_view = ProductView(
+        name=product.name,
+        description=product.description,
+        shelf=product.shelf,
+        stock=product.stock,
+        stock_notification=product.stock_notification,
+        existence_notification=product.existence_notification,
+        id=product.id
+    )
+
+    return product_view
+
+# Endpoint para obtener la lista de todos los productos
+@router.get("/products", response_model=list[ProductView])
+def get_all_products(db: Session = Depends(get_db)):
+    products = db.query(Product).all()
+    product_views = [ProductView(
+        name=product.name,
+        description=product.description,
+        shelf=product.shelf,
+        stock=product.stock,
+        stock_notification=product.stock_notification,
+        existence_notification=product.existence_notification,
+        id=product.id
+    ) for product in products]
+
+    return product_views
+
 #Endpoint para agregar productos
 @router.post("/products", response_model=None)
 def create_product(product: ProductBase, db: Session = Depends(get_db)):
@@ -36,3 +73,19 @@ def update_user(product_id: int, product: ProductBase, db: Session = Depends(get
     db.commit()
     updated_product = db.query(Product).filter(Product.id == product_id).first()
     return updated_product
+
+# Endpoint para eliminar productos
+@router.delete("/products/{product_id}", response_model=None)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    # Buscar el producto en la base de datos
+    product = db.query(Product).filter(Product.id == product_id).first()
+    
+    # Verificar si el producto existe
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Producto no encontrado")
+    
+    # Eliminar el producto de la base de datos
+    db.delete(product)
+    db.commit()
+    
+    return None
