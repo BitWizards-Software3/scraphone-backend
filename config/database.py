1a61a41e88eb0b1691c6c836b38c069ef24a32ff
DavidSalazarG <david.1701713821@ucaldas.edu.co>
2023-10-30 22:11:34 -0500

    Avances semana 1 iteración 2

--- config/database.py
+++ config/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configuración de la URL de conexión a la base de datos MySQL
+#Configuración de la URL de conexión a la base de datos MySQL
 #only config Mary userbd is admin1 for other is admin
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin1:12345@localhost:3306/scraphone"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:12345@localhost:3306/scraphone"
 
 # Creación de una instancia del motor de la base de datos
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
