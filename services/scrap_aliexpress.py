d7b564a02d6add8bc38caf1d2d86406cd68a8e79
Heosve <juanjosehenao1810@gmail.com>
2023-11-07 11:00:39 -0500

    Se realizan los cambios para las etiquetas de aliexpress debido a que la misma cambio de infrastructura

--- services/scrap_aliexpress.py
+++ services/scrap_aliexpress.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+import requests
+from bs4 import BeautifulSoup
+import json
+
 def scrape_aliexpress(search_term):   
     url = requests.get(f'https://es.aliexpress.com/w/wholesale-{search_term}.html?spm=a2g0o.productlist.search.0')
     soup = BeautifulSoup(url.content, 'html.parser')
@@ -10,21 +14,20 @@ def scrape_aliexpress(search_term):
     product_data = []
     for prod in productos:
         # Extraer el nombre del producto
-        name = prod.find("h1", class_="manhattan--titleText--WccSjUS").text.strip()
+        name = prod.find("h1", class_="multi--titleText--nXeOvyr").text.strip()
         # Extraer la imagen del producto
-        img_tag = prod.find("img", class_="manhattan--img--36QXbtQ product-img")
+        img_tag = prod.find("img", class_="multi--img--1IH3lZb product-img")
         if img_tag is not None:
             image = "https:" + img_tag['src']
-            image = img_tag['src']
-            image= 'https:'+image
         else:
-            image = 'Not found'
+           print("No se encontró la imagen del producto")
+            
         # Extraer el precio del producto
-        price_text = prod.find("div", class_="manhattan--price-sale--1CCSZfK").text.strip()
+        price_text = prod.find("div", class_="multi--price--1okBCly").text.strip()
         price_text = price_text.replace('COP', '').replace(',', '')  # Eliminar 'COP' y las comas
         price = float(price_text)  # Convertir el precio a un número
         # Extraer el enlace del producto
-        link = prod['href']
+        link= prod.find("a", class_="multi--container--1UZxxHY cards--card--3PJxwBm search-card-item").get('href')
         if link.startswith("//"):
             link = "https:" + link
 
@@ -35,6 +38,11 @@ def scrape_aliexpress(search_term):
             'Link': link,
             'Image': image
         })
+
+    # Convertir la lista de productos en un objeto JSON y devolverlo
+    return json.dumps(product_data, indent=4)
+
+print(scrape_aliexpress('iphone'))
 
     # Convertir la lista de productos en un objeto JSON y devolverlo
     return product_data
