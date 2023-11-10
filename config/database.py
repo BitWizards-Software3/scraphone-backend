e45b6356edc0cfe861049f2ec35bb2bef4e74fb1
Camilo-Caldas <juan.1701812099@ucaldas.edu.co>
2023-11-10 08:39:15 -0500

    encrypt key from cloud database

--- config/database.py
+++ config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 #Configuración de la URL de conexión a la base de datos MySQL
 #only config Mary userbd is admin1 for other is admin
-SQLALCHEMY_DATABASE_URL = "mysql://root:c156bfc35A3DbEe34GhbAc2A-13Ee413@roundhouse.proxy.rlwy.net:47095/railway" #conectar a la base de datos en la nube
+SQLALCHEMY_DATABASE_URL = "XXX desencriptar la clave de la db" #conectar a la base de datos en la nube
 #mysql://root:c156bfc35A3DbEe34GhbAc2A-13Ee413@roundhouse.proxy.rlwy.net:47095/railway
 
 #mysql+pymysql://admin:1234@localhost:3306/scraphone    con base de datos local
@@ -13,3 +13,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+""""
+encriptar clave de la base de datos en la nube
+
+1. mmysql://root:c156bfc35A3DbEe34GhbAc2A-13Ee413  
+2. zx@roundhouse.proxy.rlwy.net:
+3. 12A47095/railway
+"""
